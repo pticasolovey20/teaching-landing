c21db5ac81f4f59135992edd6a79f61808e8eae4
Dmytro Solovyov <dy.solovyov.work@gmail.com>
2023-12-15 21:08:41 +0200

    started working on creating a layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
 const Home = () => {
 	return (
-		<main>
-			<p>page is working</p>
+		<main className="min-h-[100svh] h-full bg-gray-900/90">
+			<div className="px-2 py-1">
+				<p className="text-white font-semibold">page is working</p>
+			</div>
 		</main>
 	);
 };
